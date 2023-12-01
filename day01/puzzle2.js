65589c83780fdb5e4d9eb0fec8cedc10b1d2a9a0
Filip Asprovski <filipa@CodeFuel.com>
2023-12-01 15:07:39 +0100

    advent

---
+++ day01/puzzle2.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const data = fs.readFileSync('./adventInputOne.txt', 'utf-8').split('\r\n');
+
+let sum = 0;
+const mapToNumbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9};
+
+function findNum(line, i) {
+    if(Number.isInteger(Number(line[i]))) {
+        return +line[i];
+    }
+    for(const num of Object.keys(mapToNumbers)) {
+        if(line.substring(i).indexOf(num) === 0) {
+            return mapToNumbers[num];
+        }
+    }
+    return 0;
+}
+
+for(let line of data) {
+    let num1 = 0;
+    let num2 = 0;
+    for(let i = 0; i < line.length; i++) {
+        num1 = findNum(line, i);
+        if(num1 !== 0) break;
+    }
+    for(let i = line.length - 1; i >= 0; i--) {
+        num2 = findNum(line, i);
+        if(num2 !== 0) break;
+    }
+    sum += Number(`${num1}${num2}`);
+}
+
+console.log('Final result', sum);
