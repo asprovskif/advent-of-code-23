51031d3ded76680873bef3e65d7c3ff0ebf31fe1
Filip Asprovski <filipa@CodeFuel.com>
2023-12-01 15:21:40 +0100

    add examples

--- day01/puzzle1.js
+++ day01/puzzle1.js
@@ -1,3 +1,11 @@
+// const example =
+//
+// `1abc2
+// pqr3stu8vwx
+// a1b2c3d4e5f
+// treb7uchet`
+// const data = example.split('\n');
+
 const fs = require('fs');
 const data = fs.readFileSync('./adventInputOne.txt', 'utf-8').split('\r\n');
 
--- day01/puzzle2.js
+++ day01/puzzle2.js
@@ -1,5 +1,18 @@
+// const example =
+//
+// `two1nine
+// eightwothree
+// abcone2threexyz
+// xtwone3four
+// 4nineeightseven2
+// zoneight234
+// 7pqrstsixteen`
+//
+// const data = example.split('\n');
+
 const fs = require('fs');
 const data = fs.readFileSync('./adventInputOne.txt', 'utf-8').split('\r\n');
+
 
 let sum = 0;
 const mapToNumbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9};
