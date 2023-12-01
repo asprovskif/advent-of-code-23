2a738d509fcf2136175c025503801a2eb611a2cd
Filip Asprovski <filipa@CodeFuel.com>
2023-12-01 15:10:57 +0100

    fixes

--- day01/puzzle1.js
+++ day01/puzzle1.js
@@ -4,8 +4,8 @@ const data = fs.readFileSync('./adventInputOne.txt', 'utf-8').split('\r\n');
 let sum = 0;
 
 for(const line of data) {
-    const nums = line.split('').filter(e => Number.isInteger(+e));
-    sum += +`${nums[0]}${nums[nums.length - 1]}`;
+    const nums = line.split('').filter(e => Number.isInteger(Number(e)));
+    sum += Number(`${nums[0]}${nums[nums.length - 1]}`);
 }
 
-console.log(sum);
+console.log('Final result', sum);
